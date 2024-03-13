9513cb68081331d51a262d24d34c658ef43e75f9
muratayd <murataydemir@ku.edu.tr>
2024-03-13 20:40:07 +0300

    new test data

--- project/test/13 Z/script.py
+++ project/test/13 Z/script.py
@@ -25,17 +25,26 @@ def closest_distance_to_segment(x, y, x1, y1, x2, y2):
 with open('mqtt_data.txt', 'r') as file:
     lines = file.readlines()
 
+# Read the uwb_segments from the JSON file
+with open('line_segments.json', 'r') as file:
+    uwb_segments_data = json.load(file)
+    uwb_segments = uwb_segments_data.get('segments')
+    print(uwb_segments)
+    for x1, y1, x2, y2 in uwb_segments:
+        print(distance(x1, y1, x2, y2) )
+# Calculate the total length of line segments
+total_length = sum(distance(x1, y1, x2, y2) for x1, y1, x2, y2 in uwb_segments)
+print(f"Total length of line segments: {total_length}")
+
 # Create a new Excel workbook and select the active sheet
 wb = Workbook()
 ws = wb.active
-ws.append(["Pozyx X", "Pozyx Y", "UWB Closest Distance", "Robot X", "Robot Y", "Odometry closest_distance", "Combined (80% Odometry, 20% UWB)"])
+ws.append(["Pozyx X", "Pozyx Y", "UWB Error", "Robot X", "Robot Y", "Odometry Error", "Combined (80% Odometry, 20% UWB)",
+           "Combined (20% Odometry, 80% UWB)", "Combined (50% Odometry, 50% UWB)"])
 
 # Initialize variables to store the x and y data
 pozyx_x, pozyx_y = None, None
 robot_x, robot_y = None, None
-
-# Initialize the points of the line segments
-uwb_segments = [(-1492.0, 1183.0, 1000, 1100), (1000, 1100, -1500.0, -500.0), (-1500.0, -500.0, 1000.0, -500.0)]
 
 # Iterate over the lines in the file
 i = 0
@@ -52,10 +61,13 @@ while i < len(lines):
         robot_y = robot_data.get('y')
         closest_distance_odometry = min(closest_distance_to_segment(robot_x, robot_y, *segment) for segment in uwb_segments)
         i += 2  # Skip the next line which contains the data
-        # Calculate the combined distance (80% odometry, 20% UWB)
-        combined_distance = 0.8 * closest_distance_odometry + 0.2 * closest_distance_uwb
+        # Calculate the combined distances
+        combined_distance_80_20 = 0.8 * closest_distance_odometry + 0.2 * closest_distance_uwb
+        combined_distance_20_80 = 0.2 * closest_distance_odometry + 0.8 * closest_distance_uwb
+        combined_distance_50_50 = 0.5 * closest_distance_odometry + 0.5 * closest_distance_uwb
         # Write the data to the Excel sheet
-        ws.append([pozyx_x, pozyx_y, closest_distance_uwb, robot_x, robot_y, closest_distance_odometry, combined_distance])
+        ws.append([pozyx_x, pozyx_y, closest_distance_uwb, robot_x, robot_y, closest_distance_odometry,
+                   combined_distance_80_20, combined_distance_20_80, combined_distance_50_50])
     else:
         i += 1  # Move to the next line
 
